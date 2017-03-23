fdff878782b1552c63415c4c1af9e0d3d599183b
Dennis Alund <dennis.alund@gmail.com>
2017-03-23 20:26:50 +0800

    Adding app engine app

---
+++ app-engine/main.py
@@ -0,0 +1,23 @@
+from google.appengine.api import urlfetch
+import webapp2
+import urllib
+import os
+
+class Sheduler(webapp2.RequestHandler):
+    def get(self):
+        appId = os.environ.get('GCLOUD_PROJECT', 'n-a')
+
+        try:
+            response = urlfetch.fetch(
+                url='https://us-central1-' + appId + '.cloudfunctions.net/nxSyncRequest',
+                payload=urllib.urlencode({'key': os.environ.get('CRON_API_KEY')}),
+                method=urlfetch.POST,
+                headers={'Content-Type': 'application/x-www-form-urlencoded'})
+
+            self.response.set_status(response.status_code, response.content)
+
+        except urlfetch.Error:
+            self.response.set_status(500, 'Caught exception while calling cloud functions')
+
+
+app = webapp2.WSGIApplication([('/cron', Sheduler)], debug=True)
